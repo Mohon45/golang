4a9c6475d0959345ea46eba6f5cb9aecef41577a
Mohon45 <mdmohon6145@gmail.com>
2025-10-03 10:58:08 +0600

    add function

--- function/main.go
+++ function/main.go
@@ -17,15 +17,25 @@ func getNumbers(num1 int, num2 int) (int, int) {
 }
 
 func main() {
-	a := 10
-	b := 20
+	// a := 10
+	// b := 20
 
 	// sum := add(a,b)
 
 	// fmt.Println(sum)
 
-	p, q := getNumbers(a, b)
+	// p, q := getNumbers(a, b)
 
-	fmt.Println(p)
-	fmt.Println(q)
+	// fmt.Println(p)
+	// fmt.Println(q)
+
+
+	fmt.Println("Welcome to the application!")
+
+	var name string
+
+	fmt.Println("Enter Your Name -")
+	fmt.Scanln(&name)
+
+	fmt.Println("The Input Name is:________", name)
 }
