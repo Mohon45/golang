3c9470115281627077ab788d8703f2519962b1d4
Mohon45 <mdmohon6145@gmail.com>
2025-10-02 21:44:56 -0700

    add function

---
+++ function/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func add(num1 int, num2 int) int {
+	sum := num1 + num2
+
+	return  sum
+	// fmt.Println(sum)
+}
+
+func getNumbers(num1 int, num2 int) (int, int) {
+	sum := num1 + num2
+
+	mul := num1 * num2
+	return  sum, mul
+}
+
+func main() {
+	a := 10
+	b := 20
+
+	// sum := add(a,b)
+
+	// fmt.Println(sum)
+
+	p, q := getNumbers(a, b)
+
+	fmt.Println(p)
+	fmt.Println(q)
+}
